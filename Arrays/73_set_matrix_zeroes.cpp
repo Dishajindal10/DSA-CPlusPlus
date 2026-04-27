a47ed698e8b9d404a1c107283fcb5c3160f53c6d
Dishajindal10 <dishajindal04@gmail.com>
2026-04-27 19:48:02 +0530

    solved lc 73

---
+++ Arrays/73_set_matrix_zeroes.cpp
@@ -0,0 +1,29 @@
+// Problem: 73. Set Matrix Zeroes 
+// Link: https://leetcode.com/problems/set-matrix-zeroes/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int>row(m, 0), col(n, 0);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(row[i] || col[j]) matrix[i][j] = 0;
+            }
+        }
+    }
+};
